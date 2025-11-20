a3ccda89c034b19e9a769a4fbd7a7f04cde617d7
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-11-20 17:33:26 +0200

    creating a sociallogin component

---
+++ src/components/common/SocialLogin/SocialLogin.tsx
@@ -0,0 +1,28 @@
+import Facebook from "./../../../assets/icons/facebook.png";
+import Gmail from "./../../../assets/icons/gmail.png";
+
+const SocialLogin = () => {
+  return (
+    <div className="flex flex-col gap-6">
+      <div className="px-2">
+        <p className="text-center bg-sec-text w-full h-[1px] relative ">
+          <span className="text-sec-text bg-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 px-2 py-1">
+            Or continue with
+          </span>
+        </p>
+      </div>
+      <div className="flex flex-row justify-around items-center gap-2">
+        <div className="border-2 border-light-border flex flex-row justify-center items-center py-3 w-1/2  rounded-2xl gap-3 cursor-pointer">
+          <img src={Gmail} alt="" className="w-5 h-5" />
+          <p>Google</p>
+        </div>
+        <div className="border-2 border-light-border flex flex-row justify-center items-center  py-3 w-1/2 rounded-2xl gap-3 cursor-pointer">
+          <img src={Facebook} alt="" className="w-5 h-5" />
+          <p>Facebook</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SocialLogin;
