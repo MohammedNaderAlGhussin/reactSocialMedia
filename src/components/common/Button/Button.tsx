ed21e608a72dc406547bc9ffc06bf0c2af33f6e8
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-11-20 19:26:39 +0200

    adding a shadow

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -4,7 +4,7 @@ const Button = ({ content, type }: ButtonProps) => {
   return (
     <button
       type={type}
-      className="linear-primary py-2 text-white text-lg rounded-lg cursor-pointer"
+      className="linear-primary py-2 text-white text-lg rounded-lg cursor-pointer shadow-primary-hv shadow-md mb-2"
     >
       {content}
     </button>
