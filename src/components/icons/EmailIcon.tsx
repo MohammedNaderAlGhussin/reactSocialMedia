f2bc32b39becf9de96b4ee0c59efbcd5388ac089
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-11-20 15:41:33 +0200

    adding icons components folder

---
+++ src/components/icons/EmailIcon.tsx
@@ -0,0 +1,13 @@
+const EmailIcon = () => {
+  return (
+    <svg
+      viewBox="0 0 20 20"
+      className="svg text-sec-text inline mr-2"
+      fill="currentColor"
+    >
+      <path d="M10 11.474L0 2.649V14H20V2.649L10 11.474ZM10.001 8.812L0 0V-1H20V0L10.001 8.812Z" />
+    </svg>
+  );
+};
+
+export default EmailIcon;
---
+++ src/components/icons/PasswordIcon.tsx
@@ -0,0 +1,15 @@
+const PasswordIcon = () => {
+  return (
+    <svg
+      viewBox="0 0 48 48"
+      className="svg text-sec-text inline mr-1"
+      fill="currentColor"
+    >
+      <path d="M24,25.28a3.26,3.26,0,0,0-1.64,6.07V36h3.32V31.35a3.28,3.28,0,0,0,1.61-2.8v0A3.27,3.27,0,0,0,24,25.28Z" />
+      <rect x="7.38" y="17.77" width="33.23" height="25.73" rx="4.32" />
+      <path d="M13.35,17.77V15.16a10.66,10.66,0,0,1,21.32,0v2.61" />
+    </svg>
+  );
+};
+
+export default PasswordIcon;
