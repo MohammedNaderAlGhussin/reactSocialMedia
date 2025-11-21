254ab32e67d14d7316285bc4489f56d1332eaafd
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-11-21 19:48:53 +0200

    adding the posts section

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -118,7 +118,57 @@ const Profile = () => {
             </div>
           </div>
           {/*== Profile ==*/}
-         
+          {/* Posts */}
+          <div className="pb-5">
+            <div className="bg-white mb-5 rounded-xl cursor-pointer pb-4">
+              <div className="flex items-center p-4 pb-0">
+                <img
+                  src="https://easykey.uk/images/vgift/barry-avatar-400.png"
+                  alt=""
+                  className="w-9 h-9 rounded-full"
+                />
+                <p className="flex itmes-center flex-1 flex-wrap shrink-0  text-gray-800 font-medium ml-2">
+                  Nader Al Ghussin
+                  <span className="block text-sm leading-6 text-gray-400 sm:ml-0 md:ml-1">
+                    @Nader Al Ghussin . Nov 11
+                  </span>
+                </p>
+              </div>
+              <div className="pl-8 xl:pl-16 pr-4 pt-2">
+                <p className="w-auto font-medium text-gray-800 ">
+                  Any fool can write code that a computer can understand. Good
+                  programmers write code that humans can understand.
+                  <br />
+                  <br />
+                  Experience is the name everyone gives to their
+                  <a href="#" className="text-blue-400">
+                    #mistakes
+                  </a>
+                  <br />
+                  <br />
+                  Simplicity is the soul of efficiency.
+                </p>
+                <img
+                  className="rounded-2xl border border-gray-600 my-3 mr-2 w-full"
+                  src="https://images.nature.com/original/magazine-assets/d41586-019-00653-5/d41586-019-00653-5_16459150.jpg"
+                  alt=""
+                />
+                <div className="flex justify-around items-center">
+                  <div className="flex items-center text-xs text-gray-400 hover:text-blue-400 ">
+                    likes 12.3 k
+                  </div>
+
+                  <div className="flex items-center  text-xs text-gray-400 hover:text-red-600">
+                    comments 14 k
+                  </div>
+                  <div className="flex items-center  text-xs text-gray-400 hover:text-blue-400">
+                    share
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/*== Posts ==*/}
         </div>
       </div>
     </div>
