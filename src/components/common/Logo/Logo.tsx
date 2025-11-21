751430d13e72936ba5d4ee92707d1965223a5fac
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-11-21 17:19:09 +0200

    modifying Logo style

--- src/components/common/Logo/Logo.tsx
+++ src/components/common/Logo/Logo.tsx
@@ -2,8 +2,8 @@ import logo from "./../../../assets/icons/logo.png";
 
 const Logo = () => {
   return (
-    <div className="linear w-fit text-white px-5 py-3 mb-4 rounded-xl shadow-primary-hv shadow-md">
-      <img src={logo} alt="" className="w-10 h-10" />
+    <div className="linear w-fit text-white px-4 py-3 rounded-xl shadow-primary-hv shadow-md">
+      <img src={logo} alt="logo" className="w-8 h-8" />
     </div>
   );
 };
