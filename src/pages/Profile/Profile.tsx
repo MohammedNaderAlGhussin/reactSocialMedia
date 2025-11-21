b254492f5adbb52ea039cc31da5548854185b81d
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-11-21 19:59:34 +0200

    adding the burger menu to the pages

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,3 +1,4 @@
+import BurgerMenu from "../../components/common/BurgerMenu/BurgerMenu";
 import Logo from "../../components/common/Logo/Logo";
 
 const Profile = () => {
@@ -5,52 +6,9 @@ const Profile = () => {
     <div className="min-h-screen bg-main-bg ">
       <header className="flex flex-row items-center justify-between px-5 bg-white lg:px-10 xl:px-15 fixed w-full top-0 left-0 min-h-[75px] shadow-md z-10">
         <Logo />
-        {/* <div className="relative">
-          <button
-            type="button"
-            onClick={toggleMenu}
-            className="items-center justify-center rounded-md p-2 text-gray-500 hover:bg-main-bg  focus:outline-2 focus:-outline-offset-1 focus:outline-primary cursor-pointer"
-          >
-            {show == true ? (
-              <svg
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                data-slot="icon"
-                aria-hidden="true"
-                className="w-6 h-6"
-              >
-                <path d="M6 18 18 6M6 6l12 12" />
-              </svg>
-            ) : (
-              <svg
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                data-slot="icon"
-                aria-hidden="true"
-                className="w-6 h-6"
-              >
-                <path d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-              </svg>
-            )}
-          </button>
-          {show && (
-            <div className=" px-2 py-3 absolute top-15 -right-2 w-50 text-center z-50 bg-white shadow-lg">
-              <a href="#" className="burger-menu-link">
-                Home
-              </a>
-              <a href="#" className="burger-menu-link">
-                Profile
-              </a>
-              <a href="#" className="burger-menu-link">
-                Logout
-              </a>
-            </div>
-          )}
-        </div> */}
+        <BurgerMenu />
 
-        <nav>
+        <nav className="hidden md:block">
           <ul className="flex gap-3">
             <li>
               <a href="" className="nav-link active">
