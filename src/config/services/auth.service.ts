ecb9946563fd8b07350afd08cf572aeaf30a0433
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-11-23 20:40:01 +0200

    adding auth requets functions

---
+++ src/config/services/auth.service.ts
@@ -0,0 +1,23 @@
+import type {
+  AuthResponse,
+  LoginPayload,
+  RegisterPayload,
+} from "../types/auth.types";
+import { api } from "./../axios.config";
+
+export const AuthService = {
+  register: async (payload: RegisterPayload): Promise<AuthResponse> => {
+    const res = await api.post<AuthResponse>("/register", payload);
+    return res.data;
+  },
+
+  login: async (payload: LoginPayload): Promise<AuthResponse> => {
+    const res = await api.post<AuthResponse>("/login", payload);
+    return res.data;
+  },
+
+  // 🔹 Logout (token added automatically by axios interceptor)
+  logout: async (): Promise<void> => {
+    await api.post("/logout");
+  },
+};
