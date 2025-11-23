7ca34c14113f10aaba8ec119ee6464fb2297cceb
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-11-23 19:47:18 +0200

    modifying import links

--- src/components/Posts/PostsList.tsx
+++ src/components/Posts/PostsList.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import Loader from "../common/Loader/Loader";
 import PostCard from "./PostCard";
 import { useAppDispatch, usePostsSelector } from "../../app/hooks";
-import { fetchPosts } from "../../features/posts/postsSlice";
+import { fetchPosts } from "../../features/posts/postsThunk";
 import ErrorMsg from "../common/Error/ErrorMsg";
 
 const PostsList = () => {
