37a84d9394ba8c295abfe26dd9d50585b2c83b52
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-11-23 17:56:32 +0200

    handling passed Error msg response

--- src/components/common/Error/ErrorMsg.tsx
+++ src/components/common/Error/ErrorMsg.tsx
@@ -1,8 +1,10 @@
-const ErrorMsg = () => {
+import type { ErrorMsgProps } from "./ErrorMsg.types";
+
+const ErrorMsg = ({ message }: ErrorMsgProps) => {
   return (
     <div className="flex items-center justify-center py-4">
-          <div className="bg-red-100 text-red-700 px-4 py-2 rounded-lg shadow text-center">
-              
+      <div className="bg-red-100 text-red-700 px-4 py-2 rounded-lg shadow text-center">
+        {message}
       </div>
     </div>
   );
