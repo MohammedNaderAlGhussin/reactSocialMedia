c107689851cf3f8078ecdbd2ff07bef94486450a
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-11-23 17:41:29 +0200

    creating a custom hook for posts state

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -4,3 +4,7 @@ import type { RootState, AppDispatch } from "./store";
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
 export const useAppSelector = useSelector.withTypes<RootState>();
+
+
+// Custom hook to select posts state
+export const usePostsSelector = () => useAppSelector((state) => state.posts);
