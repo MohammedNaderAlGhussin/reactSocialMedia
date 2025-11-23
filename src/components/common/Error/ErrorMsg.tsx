6003f31ead8db6e3cd71746085fa34a84903d9a1
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-11-23 17:44:12 +0200

    creating the error msg component

--- src/components/common/Error/ErrorMsg.tsx
+++ src/components/common/Error/ErrorMsg.tsx
@@ -1,10 +1,11 @@
-
 const ErrorMsg = () => {
   return (
-    <div>
-      
+    <div className="flex items-center justify-center py-4">
+          <div className="bg-red-100 text-red-700 px-4 py-2 rounded-lg shadow text-center">
+              
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ErrorMsg
+export default ErrorMsg;
