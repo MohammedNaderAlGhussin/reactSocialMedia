dba605b4d49456198fec7474f8da7d383f304d98
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-11-25 00:40:31 +0200

    creating the ThreeDotsMenu

---
+++ src/components/common/ThreeDotsMenu/ThreeDotsMenu.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+export default function ThreeDotsMenu() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div
+      className="relative inline-block text-left"
+      onClick={(e) => e.stopPropagation()}
+    >
+      {/* 3 dots button */}
+      <button
+        onClick={() => setOpen(!open)}
+        className="p-2 rounded-full hover:bg-gray-200 cursor-pointer"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 pl-1"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+        >
+          <path d="M10 4a2 2 0 11-4 0 2 2 0 014 0zm0 6a2 2 0 11-4 0 2 2 0 014 0zm0 6a2 2 0 11-4 0 2 2 0 014 0z" />
+        </svg>
+      </button>
+
+      {/* Dropdown menu */}
+      {open && (
+        <div className="absolute right-0 mt-1 w-32 bg-white border-2 border-primary rounded shadow-md z-10 ">
+          <button className="w-full text-left px-4 py-2 hover:bg-gray-100 cursor-pointer border-b border-gray-100">
+            Edit
+          </button>
+          <button className="w-full text-left px-4 py-2 hover:bg-gray-100 text-red-600 cursor-pointer">
+            Delete
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
