439b67a08fe88c74e4ee524479e539877f569bef
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-11-24 02:34:47 +0200

    modifying Error msg style

--- src/components/common/Error/ErrorMsg.tsx
+++ src/components/common/Error/ErrorMsg.tsx
@@ -2,8 +2,8 @@ import type { ErrorMsgProps } from "./ErrorMsg.types";
 
 const ErrorMsg = ({ message }: ErrorMsgProps) => {
   return (
-    <div className="flex items-center justify-center py-4">
-      <div className="bg-red-100 text-red-700 px-4 py-2 rounded-lg shadow text-center">
+    <div className="flex items-center justify-center ">
+      <div className="bg-red-100 text-red-700 px-2 py-1 rounded-lg shadow text-center">
         {message}
       </div>
     </div>
