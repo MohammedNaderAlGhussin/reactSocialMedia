4a193b7424ff619cce774ff1355dd6051774ca7f
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-11-24 02:32:51 +0200

    updating paths with protected routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -3,6 +3,7 @@ import Login from "../pages/Auth/Login";
 import Register from "../pages/Auth/Register";
 import Home from "../pages/Home/Home";
 import Profile from "../pages/Profile/Profile";
+import { ProtectedRoute } from "./ProtectedRoute";
 
 const AppRoutes = () => {
   return (
@@ -10,9 +11,27 @@ const AppRoutes = () => {
       {/* Public */}
       <Route path="/" element={<Login />} />
       <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
+      <Route path="/register" element={<Register />} />
+      {/* <Route path="/home" element={<Home />} />
+      <Route path="/profile" element={<Profile />} /> */}
+
+      {/* Protected */}
+      <Route
+        path="/home"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/profile"
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 };
