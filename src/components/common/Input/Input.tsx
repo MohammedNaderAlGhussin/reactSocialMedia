b243c345d31697fa41a0a78bc855eb30dc7f0e1c
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-11-24 15:51:41 +0200

    displaying an error msg while inputs arent validated

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -11,6 +11,7 @@ const Input = ({
   value,
   checked,
   onChange,
+  error,
 }: InputProps) => {
   const renderIcon = () => {
     if (!icon) return null;
@@ -22,35 +23,43 @@ const Input = ({
   return (
     <>
       {type === "checkbox" ? (
-        <>
-          <input
-            type={type}
-            id={id}
-            name={name}
-            checked={checked}
-            onChange={onChange}
-            className="w-[15px] sm:w-5"
-          />
-          <label
-            htmlFor={id}
-            className="text-sec-text text-[12px] md:text-[13px]"
-          >
-            {labelText}
-          </label>
-        </>
+        <div className="flex flex-col">
+          <div className="flex gap-1 items-center ">
+            <input
+              type={type}
+              id={id}
+              name={name}
+              checked={checked}
+              onChange={onChange}
+              className="w-[15px] sm:w-5"
+            />
+            <label
+              htmlFor={id}
+              className="text-sec-text text-[12px] md:text-[13px] block"
+            >
+              {labelText}
+            </label>
+          </div>
+          {error && (
+            <p className="text-red-500 text-[12px] mt-1 pl-5">{error}</p>
+          )}
+        </div>
       ) : (
         <>
-          <label
-            htmlFor={id}
-            className="text-main-text flex items-center gap-1"
-          >
-            {renderIcon()}
-            {labelText}
-          </label>
+          <div className="flex gap-2">
+            <label
+              htmlFor={id}
+              className="text-main-text flex items-center gap-1"
+            >
+              {renderIcon()}
+              {labelText}
+            </label>
+            {error && <p className="text-red-500 text-[12px] mt-1">{error}</p>}
+          </div>
           <input
             type={type}
             id={id}
-            className="input"
+            className={`input ${error ? "border-red-500" : ""}`}
             name={name}
             placeholder={placeholder}
             value={value}
