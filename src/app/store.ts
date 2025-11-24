4c1d22dc877585ef231cd6c751e03efb4dd126d8
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-11-24 02:34:15 +0200

    adding the authslice reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postsReducer from "../features/posts/postsSlice";
+import authReducer from "../features/auth/authSlice";
 
 // ...
 
 export const store = configureStore({
   reducer: {
     posts: postsReducer,
+    auth: authReducer,
   },
 });
 
