7ed476cec1f5e38b27e4ca6ac562595fe0063c6f
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-11-24 04:05:28 +0200

    implementing register to UI and handel form data

--- src/pages/Auth/Register.tsx
+++ src/pages/Auth/Register.tsx
@@ -1,12 +1,19 @@
+import { useAppDispatch, useAuthSelector } from "../../app/hooks";
+import { useState } from "react";
+import { registerThunk } from "../../features/auth/authThunk";
+
 // Importign components
 import Input from "../../components/common/Input/Input";
 import Logo from "../../components/common/Logo/Logo";
 import Button from "../../components/common/Button/Button";
 import SocialLogin from "../../components/common/SocialLogin/SocialLogin";
+import ErrorMsg from "../../components/common/Error/ErrorMsg";
+import AuthToggleLink from "../../components/common/AuthToggleLink/AuthToggleLink";
 
 // Importign types
 import type { InputProps } from "../../components/common/Input/Input.types";
 import type { ButtonProps } from "../../components/common/Button/Button.types";
+import type { AuthItem } from "../../components/common/AuthToggleLink/AuthToggleLink.types";
 
 // Importing Svg Icons
 import NameIcon from "../../components/icons/NameIcon";
@@ -14,10 +21,46 @@ import EmailIcon from "../../components/icons/EmailIcon";
 import UsernameIcon from "../../components/icons/UsernameIcon";
 import PasswordIcon from "../../components/icons/PasswordIcon";
 import ConfirmPasswordIcon from "../../components/icons/ConfirmPasswordIcon";
-import type { AuthItem } from "../../components/common/AuthToggleLink/AuthToggleLink.types";
-import AuthToggleLink from "../../components/common/AuthToggleLink/AuthToggleLink";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
+  const dispatch = useAppDispatch();
+  const { loading, error } = useAuthSelector();
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value, type, checked } = e.target;
+    setForm({ ...form, [name]: type === "checkbox" ? checked : value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (form.password !== form.confirmPassword) {
+      return <ErrorMsg message="Passwords do not match" />;
+    }
+
+    const res = await dispatch(
+      registerThunk({
+        username: form.username,
+        password: form.password,
+        name: form.name,
+        email: form.email,
+      })
+    );
+    if (res.meta.requestStatus === "fulfilled") {
+      console.log("REGISTER SUCCESS — redirect here");
+      navigate("/home");
+    }
+  };
   const inputs: InputProps[] = [
     {
       id: "name",
@@ -26,6 +69,8 @@ const Register = () => {
       placeholder: "Enter your full name",
       type: "text",
       icon: NameIcon,
+      value: form.name,
+      onChange: handleChange,
     },
     {
       id: "email",
@@ -34,14 +79,18 @@ const Register = () => {
       placeholder: "hello@example.com",
       type: "text",
       icon: EmailIcon,
+      value: form.email,
+      onChange: handleChange,
     },
     {
-      id: "user",
-      name: "user",
+      id: "username",
+      name: "username",
       labelText: "Username",
       placeholder: "Choose a username",
       type: "text",
       icon: UsernameIcon,
+      value: form.username,
+      onChange: handleChange,
     },
     {
       id: "password",
@@ -50,14 +99,18 @@ const Register = () => {
       placeholder: "Enter your Password",
       type: "password",
       icon: PasswordIcon,
+      value: form.password,
+      onChange: handleChange,
     },
     {
       id: "confrim-password",
-      name: "confrim-password",
+      name: "confirmPassword",
       labelText: "Confirm Password",
       placeholder: "Confirm your Password",
       type: "password",
       icon: ConfirmPasswordIcon,
+      value: form.confirmPassword,
+      onChange: handleChange,
     },
   ];
 
@@ -76,6 +129,7 @@ const Register = () => {
   };
   const button: ButtonProps = {
     content: "Sign Up",
+    disabled: loading,
     type: "submit",
   };
   const authLink: AuthItem = {
@@ -97,11 +151,12 @@ const Register = () => {
           <h1 className="form-title">Create an account</h1>
           <AuthToggleLink {...authLink} />
         </div>
-        <form className="flex flex-col gap-4">
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
           <div className="flex flex-col gap-2">{inputsList}</div>
           <div className="flex flex-row items-center gap-1">
             <Input {...checkbox} />
           </div>
+          {error && <ErrorMsg message={error} />}
           <Button {...button} />
         </form>
         <div className="px-2">
