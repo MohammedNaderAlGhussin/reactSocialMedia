be961020cd8b14d50e67bce4bcd2123b1f121dcd
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-11-24 02:42:44 +0200

    implementing logout and removing token/user info

--- src/components/layout/Header/Navbar.tsx
+++ src/components/layout/Header/Navbar.tsx
@@ -1,6 +1,14 @@
 import { NavLink } from "react-router-dom";
+import { useAppDispatch } from "../../../app/hooks";
+import { logoutThunk } from "../../../features/auth/authThunk";
 
 const Navbar = () => {
+  const dispatch = useAppDispatch();
+
+  const handelLogOut = () => {
+    dispatch(logoutThunk());
+  };
+
   return (
     <nav className="hidden md:block">
       <ul className="flex gap-3">
@@ -10,7 +18,7 @@ const Navbar = () => {
         <NavLink to="/profile" className="nav-link">
           Profile
         </NavLink>
-        <NavLink to="/" className="nav-link">
+        <NavLink to="/" className="nav-link" onClick={handelLogOut}>
           Logout
         </NavLink>
       </ul>
