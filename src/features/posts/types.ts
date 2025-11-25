c91dec3b95e3e35e2aeb52cda40429b0a9f6b08e
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-11-25 04:08:23 +0200

    adding types for creating/deleting status for posts

--- src/features/posts/types.ts
+++ src/features/posts/types.ts
@@ -3,5 +3,7 @@ import type { Post } from "../../config/types/post.types";
 export interface PostsState {
   posts: Post[];
   loading: boolean;
+  creating: boolean;
+  deleting: boolean;
   error: string | null;
 }
