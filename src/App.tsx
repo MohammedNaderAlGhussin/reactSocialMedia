c1098d6c68a106e604dcc7abeb1aa30ddbcb4f2f
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-11-26 06:24:59 +0200

    showing modal in  project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Modal from "./components/layout/Modal/Modal";
 import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
     <>
+      <Modal />
       <AppRoutes />
     </>
   );
