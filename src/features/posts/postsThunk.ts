9ffbbef1dbd3c078963ad07f8f11768c1c71cc6b
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-11-26 15:25:52 +0200

    creating updatePost Thunk func

--- src/features/posts/postsThunk.ts
+++ src/features/posts/postsThunk.ts
@@ -1,7 +1,10 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { PostService } from "../../config/services/post.service";
 import axios from "axios";
-import type { CreatePostPayload } from "../../config/types/post.types";
+import type {
+  CreatePostPayload,
+  UpdatePostPayload,
+} from "../../config/types/post.types";
 
 export const fetchPosts = createAsyncThunk(
   "posts/fetchPosts",
@@ -31,6 +34,25 @@ export const createPost = createAsyncThunk(
       if (axios.isAxiosError(err)) {
         return thunkAPI.rejectWithValue(
           err.response?.data?.message || err.message || "Request failed"
+        );
+      }
+      return thunkAPI.rejectWithValue("Something went wrong");
+    }
+  }
+);
+
+//  Update Post Thunk function
+
+export const updatePost = createAsyncThunk(
+  "/posts/updatePost",
+  async (payload: UpdatePostPayload, thunkAPI) => {
+    try {
+      const res = await PostService.updatePost(payload);
+      return res;
+    } catch (err: unknown) {
+      if (axios.isAxiosError(err)) {
+        return thunkAPI.rejectWithValue(
+          err?.response?.data?.message || err.message || "Failed to update post"
         );
       }
       return thunkAPI.rejectWithValue("Something went wrong");
