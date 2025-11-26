f53a55e738c6158e6f13de50e0dcda6cb4f91b05
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-11-26 05:57:29 +0200

    adding modalslice to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postsReducer from "../features/posts/postsSlice";
 import authReducer from "../features/auth/authSlice";
-
+import modalReducer from "../features/modal/modalSlice";
 // ...
 
 export const store = configureStore({
   reducer: {
     posts: postsReducer,
     auth: authReducer,
+    modal: modalReducer,
   },
 });
 
