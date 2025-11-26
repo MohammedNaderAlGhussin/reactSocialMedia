9f6cb055f63a5dbb6573f857112b5b761a023a82
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-11-26 07:11:56 +0200

    animating modal opening and closing

--- src/components/layout/Modal/Modal.tsx
+++ src/components/layout/Modal/Modal.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from "framer-motion";
 import { useAppDispatch, useModalSelector } from "../../../app/hooks";
 import { closeModal } from "../../../features/modal/modalSlice";
 import DeleteConfirm from "../../Posts/DeleteConfirm";
@@ -7,31 +8,48 @@ const Modal = () => {
   const { isOpened, type, payload } = useModalSelector();
   const dispatch = useAppDispatch();
 
-  if (!isOpened) return null;
   return (
-    <div className="fixed inset-0 bg-black/40 flex items-center justify-center z-50">
-      <div className="bg-white rounded-xl p-6 w-[90%] max-w-md shadow-xl relative">
-        {/* Close button */}
-        <button
-          onClick={() => dispatch(closeModal())}
-          className="absolute top-2 right-2 text-gray-500 hover:text-black cursor-pointer"
+    <AnimatePresence>
+      {isOpened && (
+        <motion.div
+          className="fixed inset-0 bg-black/40 flex items-center justify-center z-50"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
         >
-          ✕
-        </button>
+          <motion.div
+            className="bg-white rounded-xl p-6 w-[90%] max-w-md shadow-xl relative"
+            initial={{ opacity: 0, scale: 0.75 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.75 }}
+            transition={{ duration: 0.25 }}
+          >
+            {/* Close button */}
+            <button
+              onClick={() => dispatch(closeModal())}
+              className="absolute top-2 right-2 text-gray-500 hover:text-black cursor-pointer"
+            >
+              ✕
+            </button>
 
-        {/* Dynamic content */}
-        {type === "edit" && (
-          <EditForm post={payload} onFinish={() => dispatch(closeModal())} />
-        )}
+            {/* Dynamic content */}
+            {type === "edit" && (
+              <EditForm
+                post={payload}
+                onFinish={() => dispatch(closeModal())}
+              />
+            )}
 
-        {type === "delete" && (
-          <DeleteConfirm
-            postId={payload}
-            onFinish={() => dispatch(closeModal())}
-          />
-        )}
-      </div>
-    </div>
+            {type === "delete" && (
+              <DeleteConfirm
+                postId={payload}
+                onFinish={() => dispatch(closeModal())}
+              />
+            )}
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
 
