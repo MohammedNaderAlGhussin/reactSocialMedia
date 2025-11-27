c8f51fd5cd911aaddc39a61baee8dcb1c272e028
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-11-27 19:22:59 +0200

    modigying logo shadow

--- src/components/common/Logo/Logo.tsx
+++ src/components/common/Logo/Logo.tsx
@@ -2,7 +2,7 @@ import logo from "./../../../assets/icons/logo.png";
 
 const Logo = () => {
   return (
-    <div className="linear w-fit text-white px-4 py-3 rounded-xl shadow-primary-hv shadow-md">
+    <div className="linear w-fit text-white px-4 py-3 rounded-xl shadow-primary-hv shadow-sm">
       <img src={logo} alt="logo" className="w-8 h-8" />
     </div>
   );
