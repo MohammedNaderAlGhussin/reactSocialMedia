025625c4833d82d46cf6a4a5b5ed1462e3e19ab9
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-11-27 19:23:32 +0200

    handling theme toggler and burder menu appearance

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,13 +1,17 @@
 import Logo from "../../common/Logo/Logo";
+import ThemeToggle from "../../common/ThemeToggle/ThemeToggle ";
 import BurgerMenu from "./BurgerMenu";
 import Navbar from "./Navbar";
 
 const Header = () => {
   return (
-    <header className="flex flex-row items-center justify-between px-5 bg-white lg:px-10 xl:px-15 fixed w-full top-0 left-0 min-h-[75px] shadow-md z-10">
+    <header className="flex flex-row items-center justify-between px-5 bg-card-bg lg:px-10 xl:px-15 fixed w-full top-0 left-0 min-h-[75px] shadow-md z-10">
       <Logo />
-      <BurgerMenu />
       <Navbar />
+      <div className="flex gap-4 items-center">
+        <ThemeToggle />
+        <BurgerMenu />
+      </div>
     </header>
   );
 };
