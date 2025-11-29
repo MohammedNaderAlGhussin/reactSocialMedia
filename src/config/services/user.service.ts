66907fa970b096c46c48d83988c396283773d00d
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-11-29 02:07:58 +0200

    adding user.servcies api calls

---
+++ src/config/services/user.service.ts
@@ -0,0 +1,16 @@
+import { api } from "../axios.config";
+import type { Post } from "../types/post.types";
+import type { User } from "../types/user.types";
+
+export const UserService = {
+  getUserById: async (userId: number = 1): Promise<User> => {
+    const res = await api.get(`/users/${userId}`);
+    console.log(res.data.data);
+    return res.data.data;
+  },
+
+  getUserPosts: async (userId: number): Promise<Post[]> => {
+    const res = await api.get(`/users/${userId}/posts`);
+    return res.data.data;
+  },
+};
